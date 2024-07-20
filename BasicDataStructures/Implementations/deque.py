3d5dbf099012e34df0b15c8def261d51ec4719a8
mrcs-abr <marcos.br258@gmail.com>
2024-07-20 16:09:44 -0300

    Added deque.py

---
+++ BasicDataStructures/Implementations/deque.py
@@ -0,0 +1,28 @@
+class Deque:
+
+    def __init__(self):
+        self.items = []
+    
+    def addRear(self, item):
+        self.items.insert(0, item)
+    
+    def addFront(self, item):
+        self.items.append(item)
+
+    def removeRear(self):
+        if not self.isEmpty():
+            return self.items.pop(0)
+            
+    def removeFront(self):
+        if not self.isEmpty():
+            return self.items.pop()
+
+    def isEmpty(self):
+        return self.items == []
+
+    def size(self):
+        return len(self.items)
+
+
+
+
